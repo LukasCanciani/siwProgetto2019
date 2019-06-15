8350553a5c71a8d8a8796b89095be315d235b315
LukasCanciani-Portatile <cancianilukas@gmail.com>
2019-06-15 11:59:17 +0200

    Fixed ER

--- SilphSPA/src/main/java/it/uniroma3/CancianiQuintarelli/SilphSPA/SilphSpaApplication.java
+++ SilphSPA/src/main/java/it/uniroma3/CancianiQuintarelli/SilphSPA/SilphSpaApplication.java
@@ -3,11 +3,13 @@ package it.uniroma3.CancianiQuintarelli.SilphSPA;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SilphSpaApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SilphSpaApplication.class, args);
 	}
+	
 
 }
